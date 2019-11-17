f077bfa879e8c236714c5015f11109a8b5a1dc44
suresh_babu_sekar <suresh.babu.sekar@mheducation.com>
2019-11-17 04:58:08 -0500

    Adding testcases

--- src/test/java/com/root/service/TripProcessorTest.java
+++ src/test/java/com/root/service/TripProcessorTest.java
@@ -3,14 +3,11 @@ package com.root.service;
 import org.junit.Test;
 
 public class TripProcessorTest {
+
     @Test
     public void processFile() {
         String[] args=null;
-        //TripProcessor.main(args);//This will process the file and prints the output in console.
+        TripProcessor.main(args);//This will process the file and prints the output in console.
     }
 
-    @Test
-    public  void  validateDriver(String driverName){
-
-    }
 }
